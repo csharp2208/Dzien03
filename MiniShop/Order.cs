dadb85e10c51b45b13ff2e1f3b7fd05c064e3406
kurs <kurs@KURS-KOMPUTER>
2020-09-05 17:10:00 +0200

    minishop

---
+++ MiniShop/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    enum OrderStatus
+    {
+        NewOrder = 10, 
+        Complete = 20, 
+        Confirmed = 30,
+        Shipped = 40,
+        Returned = 50,
+        Cancelled = 60
+    }
+    class Order
+    {
+        private String orderNumber;
+        private String customerName;
+        private String shipAddress;
+        private DateTime orderDate;
+        private OrderStatus status = OrderStatus.NewOrder;
+        private byte discount = 0;
+        private List<OrderItem> items = new List<OrderItem>();
+    }
+}
---
+++ MiniShop/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniShop
+{
+    class Product
+    {
+        private int id;
+        private string name;
+        private string descr = string.Empty; // "";
+        private double price;
+
+        private bool promo = false;
+        private bool active = true;
+
+        public Product(int id, string name, double price, string descr="")
+        {
+            this.id = id; this.name = name; this.price = price; this.descr = descr;
+        }
+    }
+}
